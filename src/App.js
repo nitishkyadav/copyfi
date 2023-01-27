30b9fa82f411b2dd02ccc8f8b0bbe83ee677dacc
Nitish Kumar Yadav <engineer.nitish7115@gmail.com>
2023-01-27 13:34:53 +0530

    Fixed NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import{Footer,Blog,Possibility,Features,Whatcopyfi ,Header } from './containers';
-import{Cta,Brand,Navbar} from './components';
-import './App.css';
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  Whatcopyfi,
+  Header,
+} from "./containers";
+import { Cta, Brand } from "./components";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
-    return (
-        <div className="App">
-          <div className="gradient_bg">
-           <Navbar />
-           <Header />
-          </div>
-          <Brand />
-          <Whatcopyfi />
-          <Features />
-          <Possibility />
-          <Cta />
-          <Blog />
-          <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Whatcopyfi />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
 
-export default  App
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,58 @@
+import React, { useRef, useState } from "react";
+import { RiMenu3Line } from "react-icons/ri";
+import { RicloseLine } from "react-icons/ri";
+import logo from "../../assets/logo-new.png";
+import "./navbar.css";
+
+const Navbar = () => {
+  const [isExpanded, setIsExpanded] = useState("false");
+  const [isDataVisible, setIsDataVisible] = useState("false");
+
+  const onClickHandler = () => {
+    if (isExpanded === "false") {
+      setIsExpanded("true");
+      setIsDataVisible("true");
+    } else {
+      setIsExpanded("false");
+      setIsDataVisible("false");
+    }
+    console.log(isExpanded);
+  };
+
+  return (
+    <div className="copyfi__navbar">
+      <div className="copyfi__navbar-links_logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <button
+        className="mobile-nav-toggle"
+        aria-controls="primary-navigation"
+        aria-expanded={isExpanded}
+        onClick={onClickHandler}
+      ></button>
+      <ul
+        id="primary-navigation"
+        className="copyfi__navbar-links_container"
+        data-visible={isDataVisible}
+      >
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#Wcopyfi">About CopyFi?</a>
+        </li>
+        <li>
+          <a href="#Feature">Feature</a>
+        </li>
+        <li>
+          <a href="#blog">Blog</a>
+        </li>
+        <li>
+          <a href="#possibility">Resources</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
