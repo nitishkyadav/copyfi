b7209a9ec008f59ca12d78218959d2ac927bc012
Nitish Kumar Yadav <engineer.nitish7115@gmail.com>
2023-01-29 17:40:11 +0530

    Unmounted Tokenomics Component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
       </div>
       <AboutCopyFi />
       <Possibility />
-      <Tokenomics />
+      {/* <Tokenomics /> */}
       {/* <Cta /> */}
       {/* <Blog /> */}
       <Footer />
