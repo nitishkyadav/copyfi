c5619baf57519ef85cfbb32652dc33f83c84ef77
Nitish Kumar Yadav <engineer.nitish7115@gmail.com>
2023-01-29 17:19:51 +0530

    Tokenomics Initial Draft

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Cta } from "./components";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import AboutCopyFi from "./containers/whatcopyfi/AboutCopyFi";
+import Tokenomics from "./components/tokenomics/Tokenomics";
 const App = () => {
   return (
     <div className="App">
@@ -14,8 +15,9 @@ const App = () => {
       </div>
       <AboutCopyFi />
       <Possibility />
+      <Tokenomics />
       {/* <Cta /> */}
-      <Blog />
+      {/* <Blog /> */}
       <Footer />
     </div>
   );
--- src/components/tokenomics/Tokenomics.js
+++ src/components/tokenomics/Tokenomics.js
@@ -1,7 +1,97 @@
 import { Fragment } from "react";
+import classes from "./Tokenomics.module.css";
 
 const Tokenomics = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      {/* <div className={classes.tokenomicsContainer}> */}
+      <div className={classes.heading}>
+        <h1>$CFY Tokenomics</h1>
+      </div>
+
+      <div className={classes.tokenomics}>
+        <div
+          className={`${classes.totalSupplyContainer} ${classes.subContainer}`}
+        >
+          <div className={classes.totalSupplyDetail}>
+            Fixed Supply of <span>1B $CFY</span>
+          </div>
+          <div className={classes.totalSupplyBar} data-toggle="false"></div>
+        </div>
+
+        <div className={`${classes.presaleContainer} ${classes.subContainer}`}>
+          <div className={`${classes.barText} ${classes.presaleDetails}`}>
+            <span className={classes.percentShare}>50%</span>
+            <br></br>
+            <span className={classes.type}>Presale</span>
+            <br></br>
+            <span className={classes.supply}>500M $CFY</span>
+          </div>
+          <div className={`${classes.presaleBar} ${classes.bar}`}></div>
+        </div>
+
+        <div className={`${classes.lpContainer} ${classes.subContainer}`}>
+          <div className={classes.barText}>
+            <span className={classes.percentShare}>25%</span>
+            <br></br>
+            <span className={classes.type}>Liquidity Pool</span>
+            <br></br>
+            <span className={classes.supply}>250M $CFY</span>
+          </div>
+          <div className={`${classes.lpBar} ${classes.bar}`}></div>
+        </div>
+
+        <div
+          className={`${classes.privateSaleContainer} ${classes.subContainer}`}
+        >
+          <div className={classes.barText}>
+            <span className={classes.percentShare}>10%</span>
+            <br></br>
+            <span className={classes.type}>Private</span>
+            <br></br>
+            <span className={classes.supply}>100M $CFY</span>
+          </div>
+          <div className={`${classes.privateSaleBar} ${classes.bar}`}></div>
+        </div>
+
+        <div className={`${classes.reserveContainer} ${classes.subContainer}`}>
+          <div className={classes.barText}>
+            <span className={classes.percentShare}>5%</span>
+            <br></br>
+            <span className={classes.type}>Reserves and Development</span>
+            <br></br>
+            <span className={classes.supply}>50M $CFY</span>
+          </div>
+          <div className={`${classes.reserveBar} ${classes.bar}`}></div>
+        </div>
+
+        <div className={`${classes.cexContainer} ${classes.subContainer}`}>
+          <div className={classes.barText}>
+            <span className={classes.percentShare}>5%</span>
+            <br></br>
+            <span className={classes.type}>CEX Listing</span>
+            <br></br>
+            <span className={classes.supply}>50M $CFY</span>
+          </div>
+          <div className={`${classes.cexBar} ${classes.bar}`}></div>
+        </div>
+
+        <div
+          className={`${classes.marketingContainer} ${classes.subContainer}`}
+        >
+          <div className={classes.barText}>
+            <span className={classes.percentShare}>5%</span>
+            <br></br>
+            <span className={classes.type}>Marketing</span>
+            <br></br>
+            <span className={classes.supply}>50M $CFY</span>
+          </div>
+          <div className={`${classes.marketingBar} ${classes.bar}`}></div>
+        </div>
+      </div>
+      {/* </div> */}
+    </Fragment>
+  );
 };
 
 export default Tokenomics;
